6ba27edd7dbb581d967e70a11f1287f02d5964e2
ccmikechen <ccmikechen@gmail.com>
2017-07-04 01:13:28 +0800

    add dev.secret.exs to .gitignore

--- config/dev.secret.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-config :poselink, Poselink.Repo,
-  username: "postgres",
-  password: "postgres"
-
-config :poselink, Poselink.ActionService.NotificationAction,
-  api_key: "AAAAVYydE-E:APA91bG1zPKY0jtXGE_iptnIbCXCWgOox0rfvUqxcJo_HHHMKXmArUWumSHLCjaGmSdxmhVWMU1dTpVpOwb0piTTKaxiL8gADvFOTlyTdIb-X2X5tw4qeO8Pv_5b5TDoxWRrRXexwMu6"
