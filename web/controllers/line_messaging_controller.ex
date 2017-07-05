da611ec3f03308ef8d963d2d22d7d3bdc325a468
ccmikechen <ccmikechen@gmail.com>
2017-07-05 22:38:56 +0800

    check line callback params

--- web/controllers/line_messaging_controller.ex
+++ web/controllers/line_messaging_controller.ex
@@ -2,6 +2,8 @@ defmodule Poselink.LineMessagingController do
   use Poselink.Web, :controller
 
   def callback(conn, params) do
+    IO.puts "Line Callback"
+    IO.inspect params
     send_resp(conn, 200, "")
   end
 end
