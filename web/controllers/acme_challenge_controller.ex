a944b8a44f790a1f4d7207cf11a47bb0763f9adf
ccmikechen <ccmikechen@gmail.com>
2017-07-05 18:16:35 +0800

    modify AcmeChallengeController

--- web/controllers/acme_challenge_controller.ex
+++ web/controllers/acme_challenge_controller.ex
@@ -1,10 +1,7 @@
 defmodule Poselink.AcmeChallengeController do
   use Poselink.Web, :controller
 
-  def show(conn, %{"challenge" => "wDdXe8tgU5Cs-593pnxdsqup8dzfxG1sccR717uFTA8"}) do
+  def show(conn, %{"challenge" => challenge}) do
     send_resp(conn, 200, "TheHashInTheFile")
-  end
-  def show(conn, _) do
-    send_resp(conn, 200, "Not valid")
   end
 end
